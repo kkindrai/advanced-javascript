4d98b1ffffb8c0c3e65ba9c02f0fec5818e4f8e0
kendra.jade <kindrai.kendra@gmail.com>
2025-04-15 01:08:35 -0500

    Created lab10

---
+++ labs/lab10 - NodeJS Webserver/app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const fileSystem = require("fs");
+
+let server = http.createServer((req, res) => {
+    
+    // Read a file, send it to the locally hosted server
+    let fileName = "index.html";
+    fileSystem.readFile(fileName, (err, data) => {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(data.toString());
+        res.end();
+    })
+    
+})
+
+server.listen(3333);
+
+console.log("listening on port 3333");
